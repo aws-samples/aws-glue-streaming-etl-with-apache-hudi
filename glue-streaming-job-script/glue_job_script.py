c854ab63a879355494d771c1f01f5b5b00e54ea2
Nikhil Khokhar <46901738+khokharn-aws@users.noreply.github.com>
2022-03-07 14:52:21 +0530

    Update glue_job_script.py

--- glue-streaming-job-script/glue_job_script.py
+++ glue-streaming-job-script/glue_job_script.py
@@ -42,73 +42,68 @@ data_frame_DataSource0 = glueContext.create_data_frame.from_catalog(
     additional_options = {"inferSchema":"true","startingPosition":starting_position_of_kinesis_iterator}
 )
 
+# config
+commonConfig = {
+  'path': s3_path_hudi
+}
+
+hudiWriteConfig = {
+  'className' : 'org.apache.hudi',
+	'hoodie.table.name': hudi_table_name,
+	'hoodie.datasource.write.operation': 'upsert',
+	'hoodie.datasource.write.table.type': 'COPY_ON_WRITE',
+	'hoodie.datasource.write.precombine.field': 'date',
+	'hoodie.datasource.write.recordkey.field': 'name',
+	'hoodie.datasource.write.partitionpath.field': 'name:SIMPLE,year:SIMPLE,month:SIMPLE,day:SIMPLE',
+	'hoodie.datasource.write.keygenerator.class': 'org.apache.hudi.keygen.CustomKeyGenerator',
+	'hoodie.deltastreamer.keygen.timebased.timestamp.type': 'MIXED',
+	'hoodie.deltastreamer.keygen.timebased.input.dateformat': 'yyyy-mm-dd',
+	'hoodie.deltastreamer.keygen.timebased.output.dateformat':'yyyy/MM/dd'
+}
+     
+hudiGlueConfig = {
+	'hoodie.datasource.hive_sync.enable': 'true',
+	'hoodie.datasource.hive_sync.sync_as_datasource': 'false',
+	'hoodie.datasource.hive_sync.database': database_name,
+	'hoodie.datasource.hive_sync.table': hudi_table_name,
+	'hoodie.datasource.hive_sync.use_jdbc':'false',
+	'hoodie.datasource.write.hive_style_partitioning' : 'true',
+	'hoodie.datasource.hive_sync.partition_extractor_class': 'org.apache.hudi.hive.MultiPartKeysValueExtractor',
+	'hoodie.datasource.hive_sync.partition_fields': 'name,year,month,day'
+}
+
+combinedConf = {
+  **commonConfig, 
+  **hudiWriteConfig, 
+  **hudiGlueConfig
+}
+
 # ensure the incomong record has the correct current schema, new fresh columns are fine, if a column exists in current schema but not in incoming record then manually add before inserting
-def evolveSchema(df,table,forcecast=False):
+def evolveSchema(kinesis_df,table,forcecast=False):
     try:
         #get existing table's schema
-        original_df = spark.sql("SELECT * FROM "+table+" LIMIT 0")
+        glue_catalog_df = spark.sql("SELECT * FROM "+table+" LIMIT 0")
         #sanitize for hudi specific system columns
-        print("\n Current schema in catalog (without hudi internal columns) is :")
         columns_to_drop = ['_hoodie_commit_time', '_hoodie_commit_seqno','_hoodie_record_key','_hoodie_partition_path','_hoodie_file_name']
-        odf = original_df.drop(*columns_to_drop)
-        odf.printSchema()
-        if (df.schema != odf.schema):
-            merged_df = df.unionByName(odf, allowMissingColumns=True)
-        print("\n Schema of incoming payload from Kinesis (after merge) is : ")
+        glue_catalog_df_sanitized = glue_catalog_df.drop(*columns_to_drop)
+        if (kinesis_df.schema != glue_catalog_df_sanitized.schema):
+            merged_df = kinesis_df.unionByName(glue_catalog_df_sanitized, allowMissingColumns=True)
         return (merged_df)
     except Exception as e:
         print (e)
-        return (df)
+        return (kinesis_df)
 
 def processBatch(data_frame, batchId):
     
     if (data_frame.count() > 0):
-    
-        DataSource0 = DynamicFrame.fromDF(data_frame, glueContext, "from_data_frame")
-
-        df = DataSource0.toDF()
-        print("\n Schema of incoming payload from Kinesis is :")
-        df.printSchema()
+      
+        kinesis_dynamic_frame = DynamicFrame.fromDF(data_frame, glueContext, "from_kinesis_data_frame")
+        kinesis_data_frame = kinesis_dynamic_frame.toDF()
         
-        df = evolveSchema(df,database_name +'.'+hudi_table_name ,False)
-
-        commonConfig = {
-            'path': s3_path_hudi
-        }
-        
-        hudiWriteConfig = {
-            'className' : 'org.apache.hudi',
-            'hoodie.table.name': hudi_table_name,
-            'hoodie.datasource.write.operation': 'upsert',
-            'hoodie.datasource.write.table.type': 'COPY_ON_WRITE',
-            'hoodie.datasource.write.precombine.field': 'date',
-            'hoodie.datasource.write.recordkey.field': 'name',
-            'hoodie.datasource.write.partitionpath.field': 'name:SIMPLE,year:SIMPLE,month:SIMPLE,day:SIMPLE',
-            'hoodie.datasource.write.keygenerator.class': 'org.apache.hudi.keygen.CustomKeyGenerator',
-            'hoodie.deltastreamer.keygen.timebased.timestamp.type': 'MIXED',
-            'hoodie.deltastreamer.keygen.timebased.input.dateformat': 'yyyy-mm-dd',
-            'hoodie.deltastreamer.keygen.timebased.output.dateformat':'yyyy/MM/dd'
-        }
-     
-        hudiGlueConfig = {
-            'hoodie.datasource.hive_sync.enable': 'true',
-            'hoodie.datasource.hive_sync.sync_as_datasource': 'false',
-            'hoodie.datasource.hive_sync.database': database_name,
-            'hoodie.datasource.hive_sync.table': hudi_table_name,
-            'hoodie.datasource.hive_sync.use_jdbc':'false',
-            'hoodie.datasource.write.hive_style_partitioning' : 'true',
-            'hoodie.datasource.hive_sync.partition_extractor_class': 'org.apache.hudi.hive.MultiPartKeysValueExtractor',
-            'hoodie.datasource.hive_sync.partition_fields': 'name,year,month,day'
-        }
-
-        combinedConf = {
-            **commonConfig, 
-            **hudiWriteConfig, 
-            **hudiGlueConfig
-        }
-        
+        kinesis_data_frame = evolveSchema(kinesis_data_frame,database_name +'.'+hudi_table_name ,False)
+ 
         glueContext.write_dynamic_frame.from_options(
-            frame = DynamicFrame.fromDF(df, glueContext, "df"), 
+            frame = DynamicFrame.fromDF(kinesis_data_frame, glueContext, "evolved_kinesis_data_frame"), 
             connection_type = "custom.spark", 
             connection_options = combinedConf
         )
